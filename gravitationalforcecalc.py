3fdddf48e69a238752a7fbd92fcde4147f0f6909
serjkhon <149728454+serjkhon@users.noreply.github.com>
2026-05-20 22:19:50 -0400

    renamed file and added a few other textbook gravitational forumulas

--- gravitationalforcecalc.py
+++
@@ -1,7 +0,0 @@
-# Gravitational force calculator
-
-def gravitationalforce (m1,m2,d):
- x = ((m1 * m2) / d **2)* 6.674*10**-11
- return x
-
-print(gravitationalforce(6 * 10**24,7.3 * 10**22,3.8 * 10**8))
